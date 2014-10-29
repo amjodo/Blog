461bccd13de156c556442b8d2423684b9a1fc6f6
dave chhim <d.chhim@hotmail.com>
2014-10-29 11:05:15 -0700

    Finish video and added comments
    Figuring out more information on why mysqli is in this order, I sort of have an idea of why but I'm not completely sure.

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,6 +2,7 @@
 	//looks for database
 	require_once(__DIR__ ."/../model/database.php");
 //looks for these variables
+// in this order to buffer & so it can be read by database	
 	$connection = new mysqli($host, $username, $password);
 //checks if there is an error with the connection
 	if ($connection->connect_error) {
@@ -12,13 +13,13 @@
 //if variable exists then do the command underneath
 	if(!$exists){
 		$query = $connection->query("CREATE DATABASE $database");
-//if created print out this message
+//if created print out this message & create the database once
 
 	if($query) {
 		echo "sucessfully created database:" . $database;
 	}
 }
-//if the code above does not work print this statement
+//executed if we already have a database
 else {
 	echo "Database already exists.";
 }
