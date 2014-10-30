ca6a9965b6b628598705903c8cfc9c1a60885e12
dave chhim <d.chhim@hotmail.com>
2014-10-30 11:06:58 -0700

    Finished video 15
    Commented this code added HTML code to ALL echo statements for precaution.

--- controller/create-db.php
+++ controller/create-db.php
@@ -6,7 +6,7 @@
 	$connection = new mysqli($host, $username, $password);
 //checks if there is an error with the connection
 	if ($connection->connect_error) {
-		die("Error:" . $connection->connect_error);
+		die("<p>Error:" . $connection->connect_error). "</p>";
 	}
 //variable that checks if other variable exists
 	$exists = $connection->select_db($database);
@@ -18,12 +18,12 @@
 // create the database ONCE
 
 	if($query) {
-		echo "sucessfully created database:" . $database;
+		echo "<p>sucessfully created database:" . $database. "</p>";
 	}
 }
 //executed if we already have a database
 else {
-	echo "Database already exists.";
+	echo "<p>Database already exists.</p>";
 }
 //creating a query to make a table in database 
 //connectin has connection to database
@@ -33,9 +33,18 @@ else {
 //AUTO_INCREMENT automatically increments ids 
 $query = $connection->query("CREATE TABLE posts ("
 	."id int(11) NOT NULL AUTO_INCREMENT,"
-	. "title" varchar(255) NOT NULL,
+	. "title varchar (255) NOT NULL,"
 	. "post text NOT NULL,"
 	."PRIMARY KEY (id))");
-
+//if table was created print out this message
+//true or false statement
+if($query) {
+	echo "<p>Succesfully created table: posts</p>";
+}
+//else statement is fired when statement is false
+//handles both code and recieves hmtl codes
+else {
+	echo "<p>$connection->error</p>";
+}
 //closes connection
 $connection->close();
