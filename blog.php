a0652db19c5af6944a6c03f9d9bc2c53b834e5a9
dave chhim <d.chhim@hotmail.com>
2014-11-03 12:09:29 -0800

    Finished video 16
    commented the code, add ul tags for nav bar & lists. Now going to start css coding & media queries.

--- blog.php
+++ blog.php
@@ -3,6 +3,7 @@
   //links database to this page
   //taking all code and pasting it here
   	  require_once(__DIR__ . "/view/header.php");
+  	  require_once(__DIR__. "/view/navigation.php");
       require_once(__DIR__ . "/controller/create-db.php");
       require_once(__DIR__ . "/view/footer.php");
   //separated code to be able to maintain easily    
---
+++ view/navigation.php
@@ -0,0 +1,6 @@
+<nav> 
+	<!--unordered list that points to post file-->
+	<ul>
+		<li><a href="../post.php">Blog Post Form</a></li>	
+	</ul>
+</nav>
