96f6efe90974789657bd1967bc630846cca3f4a1
dave chhim <d.chhim@hotmail.com>
2014-11-04 11:07:18 -0800

    Finished videos 17 & 18
    Commented code fixed path and made it easier for server to find files and folders

--- blog.php
+++ blog.php
@@ -2,6 +2,7 @@
   <?php
   //links database to this page
   //taking all code and pasting it here
+  //sets it up to be able to find the page and folders
   	  require_once(__DIR__ . "/view/header.php");
   	  require_once(__DIR__. "/view/navigation.php");
       require_once(__DIR__ . "/controller/create-db.php");
